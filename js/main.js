5242d82932c1a909ec41c75c492d0ff3524d4025
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-09 16:04:53 +0200

    digita in input ricerca , visualizza chat con la stringa input nel nome

--- js/main.js
+++ js/main.js
@@ -15,6 +15,29 @@ $(document).ready (function() {
   }
   console.log(getOreMinuti());
 
+  // funzione che prende il testo digitato nell'input e lo aggiunge come messaggio alla chat
+  function inviaMessaggio (msg, txt) {
+    // memorizza il testo digitato nell'input del messaggio
+    // var inputValue = $(".input_messaggio_new").val();
+    // controlla se è stata inserita almeno una lettera nell'input
+    if (txt != "") {
+      // seleziona e clona l'elemento del messaggio
+      var elemento = $(".template .wrap_messaggio").clone();
+      // scrive nel clone il testo del messaggio preso dall'input
+      elemento.find(".text_messaggio").text(txt);
+      // scrive nel clone l'ora attuale
+      elemento.find(".ora_messaggio").text(getOreMinuti());
+      // aggiunge la classe per il messaggio interno
+      elemento.addClass(msg);
+      // appende l'elemento messaggio alla lista dei messaggi
+      $(".sideDX_main").append(elemento);
+      // pulisce l'input del messaggio
+      $(".input_messaggio_new").val("");
+      // $(".sideDX_main").scrollTop = "2000";
+    }
+  }
+
+
   // quando l'utente digita ENTER da tastiera, aggiunge il testo dell'input alla chat
   $(".input_messaggio_new").keyup(
     function(event){
@@ -37,26 +60,46 @@ $(document).ready (function() {
       setTimeout(function(){inviaMessaggio("msg_ext", "ok"); }, 1000);
   });
 
-  // funzione che prende il testo digitato nell'input e lo aggiunge come messaggio alla chat
-  function inviaMessaggio (msg, txt) {
-    // memorizza il testo digitato nell'input del messaggio
-    // var inputValue = $(".input_messaggio_new").val();
-    // controlla se è stata inserita almeno una lettera nell'input
-    if (txt != "") {
-      // seleziona e clona l'elemento del messaggio
-      var elemento = $(".template .wrap_messaggio").clone();
-      // scrive nel clone il testo del messaggio preso dall'input
-      elemento.find(".text_messaggio").text(txt);
-      // scrive nel clone l'ora attuale
-      elemento.find(".ora_messaggio").text(getOreMinuti());
-      // aggiunge la classe per il messaggio interno
-      elemento.addClass(msg);
-      // appende l'elemento messaggio alla lista dei messaggi
-      $(".sideDX_main").append(elemento);
-      // pulisce l'input del messaggio
-      $(".input_messaggio_new").val("");
-      // $(".sideDX_main").scrollTop = "2000";
-    }
-  }
+  // quando l'utente digita nell'input ricerca della sideSX,
+  // fa comprarire nella lista della chat solo i nomi delle chat che contengono la stringa digitata
+  $(".input_cerca").keyup(
+    function(event){
+       // Selezioniamo tutti i div .chat e cicliamo con .each
+      $('.chat').each(function () {
+        // memorizza in strNomeChat il nome della chat corrente
+        var strNomeChat = $(this).find(".nome").text().toLowerCase();
+        // memorizza in carDigitati i caratteri attualmente presenti nell'input ricerca
+        var carDigitati = $(".input_cerca").val().toLowerCase();
+        // verifca se il nome della chat corrente contiene la string digitata in input ricerca
+        var chat_trovata = strNomeChat.includes(carDigitati);
+        // console.log("strNomeChat: " + strNomeChat);
+        // console.log("carDigitati: " + carDigitati);
+        // console.log("includes? "+chat_trovata);
+        if (chat_trovata) {
+          console.log("SHOW");
+          // rende visibile la chat
+          $(this).removeClass("chat_hide");
+        } else {
+          // nasconde la chat
+          console.log("HIDE");
+          $(this).addClass("chat_hide");
+        }
+        var elemento = $(this);
+        console.log(elemento);
+        // var num_chat = $(this).getAttribute ("code_chat");
+        // console.log(num_chat);
+      });
+
+
+      // // controlla se ha digitato spazio come primo carattere nell'input
+      // if (event.which == 32 && $(".input_messaggio_new").val() == " ") {
+      //   // pulisce l'input del messaggio
+      //   $(".input_messaggio_new").val("");
+      // } else if (event.which == 13) {
+      //   // l'utente ha digitato invio da tasitera
+      //   inviaMessaggio("msg_int", $(".input_messaggio_new").val());
+      //   setTimeout(function(){inviaMessaggio("msg_ext", "ok"); }, 1000);
+      // };
+  })
 
 });
