8b70630fc60d99e1da2dbd243027f9c6f2f2567b
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-09 12:18:01 +0200

    aggiornata func inviaMessaggio()

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready (function() {
     var h = d.getHours();
     var m = d.getMinutes();
 
-    // per avere i minuti sempre con due muerig
+    // per avere i minuti sempre con due numeri
      if (m < 10) {
        m = "0" + String(m);
      }
@@ -31,7 +31,7 @@ $(document).ready (function() {
   )
 
   // al click sull'icona invio, aggiunge il testo dell'input alla chat
-  $(".wrap_send .fa-paper-plane").click (
+  $(".send_message").click (
     function () {
       inviaMessaggio();
   });
@@ -43,11 +43,13 @@ $(document).ready (function() {
     // controlla se è stata inserita almeno una lettera nell'input
     if (inputValue != "") {
       // seleziona e clona l'elemento del messaggio
-      var elemento = $(".template > .msg_int").clone();
+      var elemento = $(".template .wrap_messaggio").clone();
       // scrive nel clone il testo del messaggio preso dall'input
       elemento.find(".text_messaggio").text(inputValue);
       // scrive nel clone l'ora attuale
       elemento.find(".ora_messaggio").text(getOreMinuti());
+      // aggiunge la classe per il messaggio interno
+      elemento.addClass("msg_int");
       // appende l'elemento messaggio alla lista dei messaggi
       $(".sideDX_main").append(elemento);
       // pulisce l'input del messaggio
@@ -55,50 +57,5 @@ $(document).ready (function() {
       // $(".sideDX_main").scrollTop = "2000";
     }
   }
-
-
-
-
-
-
-  // var lista = [
-  //   "Comprare latte",
-  //   "Comprare uova",
-  //   "Comprare farina",
-  //   "Comprare vino"
-  // ];
-  //
-  // // clona il template, lo manipola e lo appende al <ul> per creare la lista spesa
-  // for (var i = 0; i < lista.length; i++) {
-  //   var elemento = $(".template li").clone();
-  //   elemento.append(lista[i] + " ");
-  //   $(".todo-list").append(elemento);
-  // }
-
-  // elimina il <li> al click sul relativo <span>
-  // $(".todo-list li span").click(
-  //   function () {
-  //   $(this).parent().remove();
-  // })
-
-  // $("#add-element").keyup(
-  //   function(event){
-  //     if (event.which == 13) {
-  //       var inputValue = $("#add-element").val();
-  //       if (inputValue.length > 0) {
-  //         var elemento = $(".template li").clone();
-  //         elemento.append(inputValue+" ");
-  //         $(".todo-list").append(elemento);
-  //         $("#add-element").val("");
-  //       }
-  //     };
-  //   }
-  // )
-  //
-  // $("body").on("click", ".todo-list li span",
-  //   function() {
-  //     $(this).parent().remove();
-  //   }
-  // );
 
 });
